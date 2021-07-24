13fb52ad857809314041d05758a7a2a6d7a995f3
aravindkumardhamo <87893975+aravindkumardhamo@users.noreply.github.com>
2021-07-24 16:00:12 +0530

    Add files via upload

---
+++ test-project-master/test-project/sol1.php
@@ -0,0 +1,52 @@
+<html>
+
+<body>
+<center><h2> SOLUTION WITH EXPLANATION </h2></center>
+
+
+
+<h3>SIMPLE INTEREST</h3><p>
+1. 	
+A sum of money at simple interest amounts to Rs. 815 in 3 years and to Rs. 854 in 4 years. The sum is:<br>
+
+A.Rs. 650<br>
+B.Rs. 690<br>
+C.Rs. 698<br>
+D.Rs. 700<br>
+Answer: Option C<br>
+Explanation:S.I. for 1 year = Rs. (854 - 815) = Rs. 39.<br>
+S.I. for 3 years = Rs.(39 x 3) = Rs. 117.<br>
+Principal = Rs. (815 - 117) = Rs. 698.<br></p>
+
+<p>2.How much time will it take for an amount of Rs. 450 to yield Rs. 81 as interest at 4.5% per annum of simple interest?<br>
+A.3.5 years<br>
+B.4 years<br>
+C.4.5 years<br>
+D.5 years<br>
+Answer: Option B<br>
+Explanation:Time =(100 x 81/450 x 4.5)	= 4 years.<br></p>
+
+
+
+
+
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	
+	  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  
+</head>
+<body>
+	<center>
+
+
+</body>
+</html>
+
+
+
+
+
---
+++ test-project-master/test-project/sol2.php
@@ -0,0 +1,92 @@
+<html>
+
+<body>
+<center><h2> SOLUTION WITH EXPLANATION </h2></center>
+
+
+<h3>RATIO AND PROPORTION</h3><p>
+
+1. Two numbers are respectively 20% and 50% more than a third number. The ratio of the two numbers is:<br>
+
+A.	2 : 5<br>
+B.	3 : 5<br>
+C.	4 : 5<br>
+D.	6 : 7<br>
+Answer: Option C<br>
+
+Explanation:
+
+Let the third number be x.<br>
+
+Then, first number = 120% of x =(120x/100)=(6x/5)<br>
+Second number = 150% of x =(150x/100)=(3x/2)<br>
+ Ratio of first two numbers =6x : 3x =12x : 15x = 4 : 5 </p>
+
+<p>
+2. A sum of money is to be distributed among A, B, C, D in the proportion of 5 : 2 : 4 : 3. If C gets Rs. 1000 more than D, what is B's share?<br>
+
+A.Rs. 500<br>
+B.Rs. 1500<br>
+C.Rs. 2000<br>
+D.None of these<br>
+Answer: Option C<br>
+Explanation:Let the shares of A, B, C and D be Rs. 5x, Rs. 2x, Rs. 4x and Rs. 3x respectively.<br>
+Then, 4x - 3x = 1000<br>
+x = 1000.<br>
+B's share = Rs. 2x = Rs. (2 x 1000) = Rs. 2000.<br> </p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	
+	  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  
+</head>
+<body>
+	<center>
+
+
+</body>
+</html>
